3b8bf096caf977b749307a7cfa31619c9c32f97f
AnvanshSingh <anvansh.singh@esmagico.in>
2026-02-06 12:42:31 +0530

    refactor: Externalize Firebase configuration to environment variables and update gitignore to exclude them.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCOMjtDh4fTb60-PwScH1eenxPoP8LXPsQ",
-  authDomain: "neuralexchange-b6b7f.firebaseapp.com",
-  projectId: "neuralexchange-b6b7f",
-  storageBucket: "neuralexchange-b6b7f.firebasestorage.app",
-  messagingSenderId: "466469896602",
-  appId: "1:466469896602:web:f216c3f9d06f0356a23a23",
-  measurementId: "G-NEH4DBE7WB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
